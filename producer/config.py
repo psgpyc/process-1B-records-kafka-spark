abae3d739011861424344dfeb39e627ea8940779
psgpyc <psgpyc@gmail.com>
2025-05-07 10:03:21 +0100

    fAdded 10 producer clients in parallel to achive a throughput of 350k per 10 second.

--- producer/config.py
+++ producer/config.py
@@ -7,15 +7,29 @@ SCHEMA_REGISTRY_CONF = {
 }
 
 PRODUCER_CONF = {
-    'bootstrap.servers': 'localhost:19092',
+    'bootstrap.servers': 'localhost:19092, localhost:29092, localhost:39092',
+    'batch.size': 512 * 1024, # 4200 messages each of size 110 bytes - for 10ms optimsied
+    'linger.ms': 10,
+    'queue.buffering.max.messages': 100_000,
+    'queue.buffering.max.kbytes': 30 * 1024,
+    
     'acks': 'all',      # kafka waits for acks from all in-sync replicas(ISRs) // 0,1,all(-1)
     'enable.idempotence': True, #ensures exactly once delivery when acks=all+retries // no duplicate message are writeen even when retries are enabled.
-    'retries': 5,
-    'linger.ms': 3,
-    'batch.size': 32 * 1024, #32KB
+    'retry.backoff.ms': 500,  # Retry delay in milliseconds
+    'delivery.timeout.ms': 30000,  # Max time to wait for message delivery
     'compression.type': 'snappy'   # gzip, snappy, lz4, zstd
 }
 
-TOPIC_NAME = "user-events"
+TOPIC_NAME = "psgpyc-e"
 
 SCHEMA_PATH = "schemas/user_events-v1.avsc"
+
+def get_topic_list(client):
+        return client.list_topics().topics.keys()
+
+def get_schema(schema_path):
+    with open(schema_path, 'r') as f:
+        schema = f.read()
+        return schema
+    
+
--- producer/main.py
+++ producer/main.py
@@ -1,83 +1,151 @@
 import logging
+import time
+import json
+import multiprocessing
 from pathlib import Path
-from factory import generate_user_event
+from pprint import pprint
 from confluent_kafka import Producer
-from confluent_kafka.serialization import StringSerializer, SerializationContext, MessageField
 from confluent_kafka.schema_registry import SchemaRegistryClient
 from confluent_kafka.schema_registry.avro import AvroSerializer
 from confluent_kafka.admin import AdminClient, ConfigResource, ConfigEntry, NewTopic
-from config import ADMIN_CONFIG, TOPIC_NAME, SCHEMA_REGISTRY_CONF, SCHEMA_PATH, PRODUCER_CONF
+from confluent_kafka.serialization import StringSerializer, SerializationContext, MessageField
 
+# Import functions from factory and config modules
+from factory import generate_user_event
+from config import get_topic_list,get_schema,ADMIN_CONFIG, TOPIC_NAME, SCHEMA_REGISTRY_CONF, PRODUCER_CONF
+
+
+# Initialize the AdminClient to manage kafka topics and configurations.
 client = AdminClient(ADMIN_CONFIG)
 
-def get_topic_list():
-    return client.list_topics().topics.keys()
+# Define the schema path for the Avro schema file.
+schema_path = Path(__file__).parent / 'schemas' / 'user_events-v1.avsc'
 
+NUM_PROCESS = 10
 
-if TOPIC_NAME not in get_topic_list():
-    logging.info("Topic not found. Entering creation logic......")
-    topic = NewTopic(
-        topic=TOPIC_NAME,
-        num_partitions=10, 
-        replication_factor=3)
-    try:
-        client.create_topics([topic])
-        logging.info(f"topic[{TOPIC_NAME}] successfully created..")
-    except Exception as e:
-        logging.exception(f"An error occured while creating topic[{TOPIC_NAME}]: {e}")
-else:
-    logging.info(f"Topic[{TOPIC_NAME}] already exists.. Entering kafka...")
-
-
+def pre_health_checks():
+    """
+        This function checks if the Kafka topic specified by TOPIC_NAME exists.
+        If the topic doesn't exist, it attempts to create it.
+    """
+    if TOPIC_NAME not in get_topic_list(client):
+        logging.warning("Topic not found. Entering creation logic......")
+        topic = NewTopic(
+            topic=TOPIC_NAME,
+            num_partitions=20, 
+            replication_factor=3)
+        try:
+            client.create_topics([topic])
+            logging.info(f"topic[{TOPIC_NAME}] successfully created..")
+        except Exception as e:
+            logging.exception(f"An error occured while creating topic[{TOPIC_NAME}]: {e}")
+    else:
+        logging.info(f"Topic[{TOPIC_NAME}] already exists.. Entering kafka...")
 
 
 # producer client
-def get_schema(file_name='user_events-v1.avsc'):
-    schema_path = Path(__file__).parent / 'schemas' / file_name
-    with open(schema_path, 'r') as f:
-        schema = f.read()
-        return schema
-    
+
 def delivery_report(err, msg):
+    """
+        This callback function is called once a message is delivered to Kafka.
+        It logs the success or failure of the message delivery.
+    """
     if err is not None:
         logging.error(f"Delivery failed for User record {msg.key()}: {err}")
         return
     logging.info(f'User record { msg.key()} successfully produced to {msg.topic()} [{msg.partition()}] at offset {msg.offset()}')
 
-
-def main():
+def initialise_serializer():
+    
+     # create a client to interact with confluents schema registry
     schema_registry_client = SchemaRegistryClient(SCHEMA_REGISTRY_CONF)  
-    schema_str = get_schema()
-    record = generate_user_event()
+
+    # Retrieve the Avro schema definition from a file
+    schema_str = get_schema(schema_path=schema_path)
+
+    
+
+    # Initialize the Avro serializer with the schema and schema registry client
     avro_serializer = AvroSerializer(
         schema_registry_client=schema_registry_client,
         schema_str=schema_str) 
 
+    # Simple serializer to convert the session_id key to a byte format
     string_serializer = StringSerializer()
     
+    # Initialize the Kafka producer with the provided configuration
+   
+
+    return avro_serializer, string_serializer
+
+
+
+def main(each_process):
+    """
+        Main function to produce a user event message to the Kafka topic.
+        It initializes the producer, serializes the message using Avro, and sends it to Kafka.
+    """
+
+     # create a client to interact with confluents schema registry
+    schema_registry_client = SchemaRegistryClient(SCHEMA_REGISTRY_CONF)  
+
+    # Retrieve the Avro schema definition from a file
+    schema_str = get_schema(schema_path=schema_path)
+
+    # Initialize the Avro serializer with the schema and schema registry client
+    avro_serializer = AvroSerializer(
+        schema_registry_client=schema_registry_client,
+        schema_str=schema_str) 
+
+    # Simple serializer to convert the session_id key to a byte format
+    string_serializer = StringSerializer()
+
     producer = Producer(PRODUCER_CONF)
-
-    logging.info(f"Producing the record to topic[{TOPIC_NAME}]. ^C to exit")
-
+    
+    # Initialize the Kafka producer with the provided configuration
+    start = time.time()
     try:
-        producer.produce(
-            topic=TOPIC_NAME,
-            key=string_serializer(record.get('session_id')),
-            value=avro_serializer(record, SerializationContext(TOPIC_NAME, MessageField.VALUE)),
-            on_delivery=delivery_report)
+        logging.info(f"Producing the record to topic[{TOPIC_NAME}]. ^C to exit")
+        # Generate a sample user event to be sent to Kafka
+        
+        while True:
+            record = generate_user_event()
+
+            producer.produce(
+                topic=TOPIC_NAME,
+                key=string_serializer(record.get('session_id')), #serialize the key
+                value=avro_serializer(record, SerializationContext(TOPIC_NAME, MessageField.VALUE)),   #serialize the value
+                on_delivery=delivery_report)
+            
+            producer.poll(0)
+
+        producer.flush()
+    
     except Exception as e:
-        logging.error(f"An error has occured: {e}")
+        logging.error(f"An error has occured inside producer client: {e}")
 
-    logging.info("\nFlushing records...")
-    producer.flush()
-
-
-if __name__ == '__main__':
+    end = time.time()
+    print(f"time taken by process:{each_process} {end-start}")
 
     
+if __name__ == '__main__':
 
     logging.basicConfig(level=logging.INFO)
 
     logging.info("Start..")
 
-    main()
+    pre_health_checks()
+
+    processes = []
+
+
+    for each_process in range(NUM_PROCESS):
+        p = multiprocessing.Process(target=main, args={each_process})
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    print("completed")
+
